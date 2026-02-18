46a9abb17bdeb1113ff755d401ec6c15a4e26006
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-18 12:22:59 +0500

    Added NotFound, PostDel, Edit and NewPost Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,24 @@ import Form from "./components/Form";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
+import NotFound from "./components/NotFound";
+import Home from "./pages/Home";
+import NewPost from "./pages/NewPost";
+import PostDel from "./components/PostDel";
+import Edit from "./components/Edit";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/create-post" element={<NewPost />} />
+        <Route path="/delete-post/:id" element={<PostDel />} />
+        <Route path="/edit-post/:id" element={<Edit />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
