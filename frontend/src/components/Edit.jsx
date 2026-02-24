fac43a59a5dad60f608fdddfaee56007a98a6965
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-24 15:20:30 +0500

    Create handleUpdate function

--- frontend/src/components/Edit.jsx
+++ frontend/src/components/Edit.jsx
@@ -1,25 +1,41 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import { data } from "../components/data";
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-const Edit = () => {
+const Edit = ({ informations, setInformations }) => {
   const { id } = useParams();
-  const item = data.filter((i) => i.id === Number(id));
-  console.log(item);
+  const navigate = useNavigate();
+  const item = informations.find((i) => i.id === Number(id));
+  const [content, setContent] = useState(item ? item.content : "");
+
+  const handleUpdate = () => {
+    const updatedList = informations.map((info) =>
+      String(info.id) === String(id) ? { ...info, content: content } : info,
+    );
+
+    setInformations(updatedList);
+    navigate("/");
+  };
   return (
     <div className="container-sm mt-4">
       <div className="col my-3">
         <div className="card h-100 shadow border-0">
-          <div className="card-header bg-light text-muted">{item[0].date}</div>
+          <div className="card-header bg-light text-muted">{item.date}</div>
           <div className="card-body d-flex flex-column">
-            <h5 className="card-title fw-bold">{item[0].title}</h5>
-            <div className="card-text text-secondary">
-              <div className="transition-wrapper collapsed">
-                {item[0].content}
-              </div>
+            <h5 className="card-title fw-bold">{item.title}</h5>
+            <div className="mb-3">
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+              ></textarea>
             </div>
             <div className="d-flex justify-content-between mt-auto pt-3">
-              <button className="btn btn-sm btn-outline-primary px-3">
+              <button
+                className="btn btn-sm btn-outline-primary px-3"
+                onClick={handleUpdate}
+              >
                 Update
               </button>
             </div>
