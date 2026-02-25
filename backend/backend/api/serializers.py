3a4e3a26c9de6e54b5ca71b09c6120f407cb81f0
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-25 12:07:26 +0500

    Created register serializer

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from .models import TodoList
+from django.contrib.auth.models import User
 
 class TodoSerializer(serializers.ModelSerializer):
+    """
+    Todo List serializer for Lists items
+    """
     class Meta:
         model = TodoList
         fields = "__all__"
         read_only_fields = ['user']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    """
+    User creation serializer
+    """
+    class Meta:
+        model = User
+        fields = ["id","username", "password", "email"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
