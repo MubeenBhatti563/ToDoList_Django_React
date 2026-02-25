25b580c3490db51a516f00ea4f557f59cda2db7b
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-25 12:08:53 +0500

    Create custom token pairs for jwt

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoView, TodoOneView
+from .views import TodoView, TodoOneView, CreateUser
 
 urlpatterns = [
     path("lists/", TodoView.as_view(), name="list-create"),
-    path("lists/<int:pk>", TodoOneView.as_view(), name="list-detail")
+    path("lists/<int:pk>", TodoOneView.as_view(), name="list-detail"),
+    path("user/register/", CreateUser.as_view(), name="user-registration")
 ]
