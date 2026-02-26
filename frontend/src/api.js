f97e8d08393ac225f36c64893292a1bd04ea51d3
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-26 20:17:00 +0500

    Solve a bug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: baseURL,
 });
 
 // REQUEST INTERCEPTERS: attack the token with request
@@ -18,27 +19,30 @@ api.interceptors.request.use(
 
 // RESPONCE INTERCEPTERS: handling 401 (Expired Token)
 api.interceptors.response.use(
-  (responce) => responce,
+  (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.responce.status === 401 && !originalRequest._retry) {
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/api/token/")
+    ) {
       originalRequest._retry = true;
-    }
-    try {
-      const refreshToken = localStorage.getItem("refresh_token");
-      const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/token/refresh/`,
-        {
+      try {
+        const refreshToken = localStorage.getItem("refresh_token");
+        const res = await axios.post(baseURL, {
           refresh: refreshToken,
-        },
-      );
-      if (res.status === 200) {
-        localStorage.setItem("access_token", res.data.access);
-        return api(originalRequest);
+        });
+        if (res.status === 200) {
+          localStorage.setItem("access_token", res.data.access);
+          return api(originalRequest);
+        }
+      } catch (err) {
+        localStorage.clear();
+        window.location.href = "/login";
+        return Promise.reject(err);
       }
-    } catch {
-      localStorage.clear();
-      window.location.href = "/login";
     }
     return Promise.reject(error);
   },
