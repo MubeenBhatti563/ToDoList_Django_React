bfb0e75a74bf7de49a234084c1811089c12abc14
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-27 21:44:30 +0500

    Made refresh token to AlloyAny

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import MyTokenObtainPairView
+from rest_framework.permissions import AllowAny
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
     path('api/', include('api.urls'))
 ]
