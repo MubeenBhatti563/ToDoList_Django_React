cd856379dd087bb86d3d7e289811ca0c3477ce67
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-01 12:08:20 +0500

    Inserted ListsProvider in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Form from "./components/Form";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
@@ -8,60 +7,29 @@ import NotFound from "./components/NotFound";
 import Home from "./pages/Home";
 import NewPost from "./pages/NewPost";
 import Edit from "./components/Edit";
-import { useEffect, useState } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
-import api from "./api";
+import ListsContext from "./components/ListsContext";
 
 function App() {
-  const [informations, setInformations] = useState([]);
-  // const isLoggedIn = !!localStorage.getItem("access_token");
-  const fetchData = async () => {
-    try {
-      const token = localStorage.getItem("access_token");
-      if (!token) return;
-      const res = await api.get("/api/lists/");
-      setInformations(res.data);
-    } catch (err) {
-      console.error("Fetch Error:", err.response?.status, err.message);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
-    <>
-      <Navbar setInformations={setInformations} />
+    <ListsContext>
+      <Navbar />
       <Routes>
         <Route
           path="/"
           element={
             <ProtectedRoute>
-              <Home
-                informations={informations}
-                setInformations={setInformations}
-                fetchData={fetchData}
-              />
+              <Home />
             </ProtectedRoute>
           }
         />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/create-post"
-          element={<NewPost setInformations={setInformations} />}
-        />
-        <Route
-          path="/edit-post/:id"
-          element={
-            <Edit
-              informations={informations}
-              setInformations={setInformations}
-            />
-          }
-        />
+        <Route path="/create-post" element={<NewPost />} />
+        <Route path="/edit-post/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </ListsContext>
   );
 }
 
