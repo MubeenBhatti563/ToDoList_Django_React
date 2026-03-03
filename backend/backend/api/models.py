4d8a8a39b3e37094507c745774674afd5e7f9fca
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-03 20:20:40 +0500

    Create Like and Comment models

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -22,3 +22,50 @@ class TodoList(models.Model):
 
     def __str__(self):
         return f"{self.title} | {self.user.username}"
+    
+class Comment(models.Model):
+    """
+    Comments on each post by user
+    """
+    comment = models.CharField(max_length=250)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_comment"
+    )
+    post = models.ForeignKey(
+        TodoList,
+        on_delete=models.CASCADE,
+        related_name="post_comment"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+    
+    def __str__(self):
+        return self.comment
+    
+class Like(models.Model): # Singular name
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="user_like"
+    )
+    post = models.ForeignKey(
+        TodoList, 
+        on_delete=models.CASCADE, 
+        related_name="post_like"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'post'], 
+                name='unique_user_like'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
