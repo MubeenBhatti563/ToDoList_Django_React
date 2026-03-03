14e3c70f09ec00387d0b532599dcde294c665c97
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-03 20:22:50 +0500

    Created urls of Like and Comments

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import TodoView, TodoOneView, CreateUser
+from .views import TodoView, TodoOneView, CreateUser, CommentView, LikeView
 
 urlpatterns = [
+    # Todo Paths
     path("lists/", TodoView.as_view(), name="list-create"),
-    path("lists/<int:pk>", TodoOneView.as_view(), name="list-detail"),
-    path("user/register/", CreateUser.as_view(), name="user-registration")
+    path("lists/<int:pk>/", TodoOneView.as_view(), name="list-detail"),
+    # User Registration
+    path("user/register/", CreateUser.as_view(), name="user-registration"),
+    # Comments (Remove <int:pk> here to allow filtering via ?post=ID)
+    path("comments/", CommentView.as_view(), name="comment-list-create"),
+    # Likes (Remove <int:pk> here to allow filtering via ?post=ID)
+    path("likes/", LikeView.as_view(), name="like-list-create"),
 ]
