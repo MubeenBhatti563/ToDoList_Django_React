be0c3cad862edf4f16c7705345c95a8c8f95e9b6
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-03 20:21:14 +0500

    Created serializer for Like and Comment

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
-from .models import TodoList
+from .models import TodoList, Comment, Like
 from django.contrib.auth.models import User
 
 class TodoSerializer(serializers.ModelSerializer):
     """
     Todo List serializer for Lists items
     """
+    username = serializers.ReadOnlyField(source="user.username")
+    likes_count = serializers.IntegerField(source="post_like.count", read_only=True)
+    comments_count = serializers.IntegerField(source="post_comment.count", read_only=True)
     class Meta:
         model = TodoList
-        fields = "__all__"
+        fields = ["id", "title", "content", "username", "user", "likes_count", "comments_count", "created_at"]
         read_only_fields = ['user']
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -25,3 +28,22 @@ class UserCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for comment of user on post
+    """
+    username = serializers.ReadOnlyField(source="user.username")
+    class Meta:
+        model = Comment
+        fields = ["id", "comment", "username", "user", "post", "created_at"]
+        read_only_fields = ["user"]
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for like of user on post
+    """
+    class Meta:
+        model = Like
+        fields = "__all__"
+        read_only_fields = ["user"]
